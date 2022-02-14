480bc1ebf80307cfc0fb82779b45e76e33548e4e
sapir <sapir.s@info.cts.org.il>
2022-02-14 22:57:27 +0200

    final commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     console.log("ready")
-    var searchData, item, title, author, publisher, summery, bookLink, BookImg;
+    var searchData, item;
     var placeHolder = 'https://via.placeholder.com/300'
     var outputList = document.getElementById("Results")
     var bookUrl = `https://www.googleapis.com/books/v1/volumes?q=`
@@ -44,33 +44,33 @@ $(document).ready(function(){
     
     //handle the result google api returns through the ajax request
     const displayResults = (res) => {
+        var item1, title1, author1, publisher1, summery1, bookLink1, BookImg1;
+        var item2, title2, author2, publisher2, summery2, bookLink2, BookImg2;
         var size = res.items.length < 10 ? res.items.length : 10
         for(var i = 0; i < size; i+=2){
 
-            outputList.innerHTML += "<div class='Results__pair'>"
+            item1 = res.items[i];
+            title1 = item1.volumeInfo.title;
+            author1 = item1.volumeInfo.authors;
+            publisher1 = item1.volumeInfo.publisher;
+            summery1 = item1.searchInfo.textSnippet;
+            bookLink1 = item1.searchInfo.previewLink;
+            BookImg1 = (item1.volumeInfo.imageLinks) ? item1.volumeInfo.imageLinks.thumbnail : placeHolder;
+            bookIsbn1 = item1.volumeInfo.industryIdentifiers[1].identifier;
 
-            item = res.items[i];
-            title = item.volumeInfo.title;
-            author = item.volumeInfo.authors;
-            publisher = item.volumeInfo.publisher;
-            summery = item.searchInfo.textSnippet;
-            bookLink = item.searchInfo.previewLink;
-            BookImg = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHolder;
-            bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier;
+            item2 = res.items[i+1];
+            title2 = item2.volumeInfo.title;
+            author2 = item2.volumeInfo.authors;
+            publisher2 = item2.volumeInfo.publisher;
+            summery2 = item2.searchInfo.textSnippet;
+            bookLink2 = item2.searchInfo.previewLink;
+            BookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHolder;
+            bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier;
 
-            outputList.innerHTML += formatOutput(BookImg, title, author, publisher, summery, bookLink, bookIsbn)
-
-            item = res.items[i+1];
-            title = item.volumeInfo.title;
-            author = item.volumeInfo.authors;
-            publisher = item.volumeInfo.publisher;
-            summery = item.searchInfo.textSnippet;
-            bookLink = item.searchInfo.previewLink;
-            BookImg = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHolder;
-            bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier;
-
-            outputList.innerHTML += formatOutput(BookImg, title, author, publisher, summery, bookLink, bookIsbn)
-            outputList.innerHTML += "</div>"
+            outputList.innerHTML += ` <div class='Results__pair'> 
+                                        <li> ${formatOutput(BookImg1, title1, author1, publisher1, summery1, bookLink1, bookIsbn1)} </li>
+                                        <li> ${formatOutput(BookImg2, title2, author2, publisher2, summery2, bookLink2, bookIsbn2)} </li>
+                                      </div>`
         }
     }
 
@@ -78,27 +78,27 @@ $(document).ready(function(){
         // var viewerUrl = "book.html?isbn=" + bookIsbn
         var htmlCard = `
         <div class="Results__counter">
-        <div class="card mb-3" style="max-width: 540px; width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src=${BookImg}
-                class="img-fluid rounded-start" 
-                alt="..."/>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">${title}</h5>
-                  <p class="card-text">
-                    <b>Author:</b>  ${author}<br/>
-                    <b>Publisher:</b>  ${publisher}<br/>
-                    <br/>
-                    <b>Summery:</b>  ${summery}
-                  </p>
+            <div class="card mb-3" style="max-width: 540px; width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src=${BookImg}
+                        class="img-fluid rounded-start" 
+                        alt="..."/>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text">
+                                <b>Author:</b>  ${author}<br/>
+                                <b>Publisher:</b>  ${publisher}<br/>
+                                <br/>
+                                <b>Summery:</b>  ${summery}
+                            </p>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-    </div>
+        </div>
     `
     //<a target="_black" href="${viewerUrl}" class="btn btn-secondary">Read</a>
     return htmlCard;
